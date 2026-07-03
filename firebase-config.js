996b1a60e7b4f2952b9410ec5960f0da006541c1
edenfamilygraceltd-crypto <edenfamilygraceltd@gamil.com>
2026-07-03 20:16:21 +0200

    Update project

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,16 @@
 // Secure Firebase Configuration
 // This file should be loaded securely and not exposed to clients
 
+const env = (typeof process !== 'undefined' && process.env) ? process.env : {};
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY || "AIzaSyApUFNELOfgIe7rWEek9GLS9EIphNW09-A",
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "edensmart-app.firebaseapp.com",
-  databaseURL: process.env.FIREBASE_DATABASE_URL || "https://edensmart-app-default-rtdb.firebaseio.com",
-  projectId: process.env.FIREBASE_PROJECT_ID || "edensmart-app",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "edensmart-app.firebasestorage.app",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "1093120876724",
-  appId: process.env.FIREBASE_APP_ID || "1:1093120876724:web:bc37448cadd18d651c77e1",
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID || "G-1FL70PZZSW"
+  apiKey: env.FIREBASE_API_KEY || "AIzaSyApUFNELOfgIe7rWEek9GLS9EIphNW09-A",
+  authDomain: env.FIREBASE_AUTH_DOMAIN || "edensmart-app.firebaseapp.com",
+  databaseURL: env.FIREBASE_DATABASE_URL || "https://edensmart-app-default-rtdb.firebaseio.com",
+  projectId: env.FIREBASE_PROJECT_ID || "edensmart-app",
+  storageBucket: env.FIREBASE_STORAGE_BUCKET || "edensmart-app.firebasestorage.app",
+  messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID || "1093120876724",
+  appId: env.FIREBASE_APP_ID || "1:1093120876724:web:bc37448cadd18d651c77e1",
+  measurementId: env.FIREBASE_MEASUREMENT_ID || "G-1FL70PZZSW"
 };
 
 // Export for Node.js environments
