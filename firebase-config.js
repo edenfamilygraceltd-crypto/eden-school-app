04732d944867e1c82128823e1eac48d956565f58
edenfamilygraceltd-crypto <edenfamilygraceltd@gamil.com>
2026-07-13 13:35:35 +0200

    Update parent info

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@
 // This file should be loaded securely and not exposed to clients
 
 const env = (typeof process !== 'undefined' && process.env) ? process.env : {};
-const firebaseConfig = {
+const _firebaseConfig = {
   apiKey: env.FIREBASE_API_KEY || "AIzaSyApUFNELOfgIe7rWEek9GLS9EIphNW09-A",
   authDomain: env.FIREBASE_AUTH_DOMAIN || "edensmart-app.firebaseapp.com",
   databaseURL: env.FIREBASE_DATABASE_URL || "https://edensmart-app-default-rtdb.firebaseio.com",
@@ -15,10 +15,10 @@ const firebaseConfig = {
 
 // Export for Node.js environments
 if (typeof module !== 'undefined' && module.exports) {
-  module.exports = firebaseConfig;
+  module.exports = _firebaseConfig;
 }
 
-// Export for browser environments
+// Export for browser environments (do not create a global `firebaseConfig` identifier)
 if (typeof window !== 'undefined') {
-  window.FIREBASE_CONFIG = firebaseConfig;
+  window.FIREBASE_CONFIG = _firebaseConfig;
 }
